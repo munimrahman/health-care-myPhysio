038cf331ce783f57b254b79a272facab874d845a
munimrahman <munimrh@gmail.com>
2021-10-18 02:03:32 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDLoQLWIV5L3gNn0aEvY7ddt0CnGWF-Mcc",
+    authDomain: "health-care-munimrh.firebaseapp.com",
+    projectId: "health-care-munimrh",
+    storageBucket: "health-care-munimrh.appspot.com",
+    messagingSenderId: "746766225205",
+    appId: "1:746766225205:web:77a0e3b2caa1f92db7467c"
+};
+
+export default firebaseConfig;
